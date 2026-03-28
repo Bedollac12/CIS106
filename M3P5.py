966105527ebe0b9579e7019029ae47c44efd2c50
Christian Bedolla <bedollachristian57@gmail.com>
2026-03-28 10:13:51 -0500

    Add files via upload

---
+++ M3P5.py
@@ -0,0 +1,7 @@
+First_exam_score=float(input("Enter your first exam score: "))
+Second_exam_score=float(input("Enter your second exam score: "))
+Calculate_First_exam_score=First_exam_score*0.60
+Calculate_Second_exam_score=Second_exam_score*0.40
+Calculate_total_score=Calculate_First_exam_score+Calculate_Second_exam_score
+print(f"Your total score is: {Calculate_total_score:.2f}")
+
